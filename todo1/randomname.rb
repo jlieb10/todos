77a537f6554c702528369d1bae53b6a61fb5f54a
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-05-01 09:27:00 -0400

    Random Name Ruby File Added

---
+++ todo1/randomname.rb
@@ -0,0 +1,15 @@
+def randomname
+	randname = [1, 2, 3, 4].sample
+	case randname
+		when 1
+			puts "Mandy Yeung"
+		when 2
+			puts "April Rabkin"
+		when 3
+			puts "Kate Brender"
+		when 4
+			puts "Joshua Lieberman"
+	end
+end
+
+randomname
